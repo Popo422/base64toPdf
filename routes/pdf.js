34ffff66a3a8725515d7d15bfd8d7ae652bdba47
Zernie Reyes <zernie@Zernies-MacBook-Pro.local>
2024-09-30 07:33:21 +0800

    first commit

---
+++ routes/pdf.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var router = express.Router();
+var fs = require("fs");
+
+/* GET users listing. */
+router.post("/", function (req, res, next) {
+  try {
+    const { base64, fileName, contentType } = req.body;
+    var buf = Buffer.from(base64, "base64");
+    // Your code to handle buffer
+
+    console.log("The file was saved!");
+    res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
+    res.setHeader("Content-Type", contentType);
+    res.send(buf);
+
+    // Create a new PDF document
+    // res.setHeader(`Content-Disposition', 'attachment; filename="${fileName.replace(/ /g, '_')}.pdf`);
+  
+  } catch (error) {
+    console.log(error);
+    return new Response("error", { status: 500 });
+  }
+});
+
+module.exports = router;
